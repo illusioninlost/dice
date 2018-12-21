7ea6276e405694948d960f0760e059dd0ae77b85
Richard Chang <rchang9999@yahoo.com>
2018-12-21 17:36:28 -0500

    spacing

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.getElementById("dice1").setAttribute("src", `images/dice${randomNumber1}.png`);
 document.getElementById("dice2").setAttribute("src", `images/dice${randomNumber2}.png`);
 
-
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "✔ Player 1 wins!";
 } else if (randomNumber2 > randomNumber1) {
