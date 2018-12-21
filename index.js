312a23050b0a3a179acef63201d230a0a6b34734
Richard Chang <rchang9999@yahoo.com>
2018-12-21 17:01:24 -0500

    beautify

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var randomNumber1 = Math.floor(Math.random()*6) + 1;
-var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 document.getElementById("dice1").setAttribute("src", `images/dice${randomNumber1}.png`);
 document.getElementById("dice2").setAttribute("src", `images/dice${randomNumber2}.png`);
 
 
-if (randomNumber1 > randomNumber2){
+if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 wins.";
-}else if (randomNumber2 > randomNumber1){
+} else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 wins.";
-}else{
+} else {
   document.querySelector("h1").innerHTML = "It is a tie."
 }
